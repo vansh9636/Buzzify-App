b79e85b103676cae93c17bb9f4656a40b07f3281
vansh9636 <kumawatvansh365@gmail.com>
2026-02-03 23:36:27 +0530

    backend changes

--- BuzzifyB/App.js
+++ BuzzifyB/App.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require("cors")
 const cookieParser = require("cookie-parser");
 const connection = require("./config/dbConnection");
+const port = process.env.PORT || 8000;
 require('dotenv').config()
 
 // connect to database
@@ -15,13 +16,17 @@ const userRouter = require("./routes/userRouter");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(
-    { origin: "http://localhost:5173", credentials: true }
+    { origin: ["http://localhost:5173","https://buzzify-app-xi.vercel.app"], credentials: true }
 ));
 app.use(cookieParser());
+app.set("trust proxy", 1);
 
 //use router
+app.get("/", (req, res) => {
+    res.send("API running...");
+})
 app.use("/user", userRouter);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`)
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 })
