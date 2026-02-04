38c63c4432126e4a162f6424cdece3a8ac9781d4
vansh9636 <kumawatvansh365@gmail.com>
2026-02-04 13:17:26 +0530

    frontend env setup

--- BuzzifyF/src/Services/Axios.js
+++ BuzzifyF/src/Services/Axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_API_URL = import.meta.env.BASE_API_URL;
+const BASE_API_URL = import.meta.env.VITE_API_URL;
 
 const axiosInstance = axios.create({
   baseURL: BASE_API_URL,
