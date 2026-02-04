243eb56ae6477f9a6088aa574c540101fb9485c2
vansh9636 <kumawatvansh365@gmail.com>
2026-02-04 11:19:40 +0530

    axios url change

--- BuzzifyF/src/Services/Axios.js
+++ BuzzifyF/src/Services/Axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_API_URL = import.meta.env.VITE_BASE_API_URL || "http://localhost:8000/user";
+const BASE_API_URL = import.meta.env.BASE_API_URL || "http://localhost:8000/user";
 
 const axiosInstance = axios.create({
   baseURL: BASE_API_URL,
